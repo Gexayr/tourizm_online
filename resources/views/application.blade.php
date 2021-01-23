672c0ea3bac8c2b2efc3fdf4dbaf4e93efaeebfe
/usr/bin/nano <support@beget.ru>
2021-01-23 15:03:32 +0300

    firs init

---
+++ resources/views/application.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- <link rel="icon" href="<%= BASE_URL %>favicon.ico"> -->
+
+    <title>Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset(mix('css/main.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/iconfont.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/material-icons/material-icons.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/vuesax.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/prism-tomorrow.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/logo/favicon.png') }}">
+  </head>
+  <body>
+    <noscript>
+      <strong>We're sorry but Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    </noscript>
+    <div id="app">
+    </div>
+
+    <!-- <script src="js/app.js"></script> -->
+    <script src="{{ asset(mix('js/app.js')) }}"></script>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(71411992, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/71411992" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+  </body>
+</html>
